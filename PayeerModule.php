fae63c2e56616bde2faf74acebc945b959937d8d
shop-go <24-shops@mail.ru>
2016-12-06 20:14:59 +0100

    fix

--- PayeerModule.php
+++ PayeerModule.php
@@ -24,136 +24,134 @@ use yupe\components\WebModule;
  */
 class PayeerModule extends WebModule
 {
-  /**
-   * 
-   */
-  const VERSION='0.6';
+    /**
+     * 
+     */
+    const VERSION='0.6';
 
-  /**
-   * @return array
-   */
-  public function getDependencies()
-  {
-    return ['payment'];
-  }
+    /**
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return ['payment'];
+    }
 
-  /**
-   * @return array
-   */
-  public function getNavigation()
-  {
-    return false;
-    /*return [
-      [
-        'icon'  => 'fa fa-fw fa-list-alt',
-        'label' => 'Баланс кошелька',
-        'url'   => [
-          '/payeer/payeerBackend/index'
-        ]
-      ],
-    ];*/
-  }
+    /**
+     * @return array
+     */
+    public function getNavigation()
+    {
+        return false;
+        /*return [
+            [
+                'icon'  => 'fa fa-fw fa-list-alt',
+                'label' => 'Баланс кошелька',
+                'url'   => [
+                    '/payeer/payeerBackend/index'
+                ],
+            ],
+        ];*/
+    }
 
-  /**
-   * @return bool
-   */
-  public function getAdminPageLink()
-  {
-    return false;
-    //return '/payeer/payeerBackend/index';
-  }
+    /**
+     * @return bool
+     */
+    public function getAdminPageLink()
+    {
+        return false;
+        //return '/payeer/payeerBackend/index';
+    }
 
-  /**
-   * @return bool
-   */
-  /*public function getIsShowInAdminMenu()
-  {
-    return true;
-  }*/
+    /**
+     * @return bool
+     */
+    /*public function getIsShowInAdminMenu()
+    {
+        return true;
+    }*/
 
-  /**
-   * @return string
-   */
-  public function getVersion()
-  {
-    return self::VERSION;
-  }
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return self::VERSION;
+    }
 
-  /**
-   * @return array
-   */
-  public function getEditableParams()
-  {
-    return [];
-  }
+    /**
+     * @return array
+     */
+    public function getEditableParams()
+    {
+        return [];
+    }
 
-  /**
-   * @return string
-   */
-  public function getCategory()
-  {
-    return Yii::t('PayeerModule.payeer','Store');
-  }
+    /**
+     * @return string
+     */
+    public function getCategory()
+    {
+        return Yii::t('PayeerModule.payeer', 'Store');
+    }
 
-  /**
-   * @return string
-   */
-  public function getName()
-  {
-    return Yii::t('PayeerModule.payeer','Payeer');
-  }
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return Yii::t('PayeerModule.payeer', 'Payeer');
+    }
 
-  /**
-   * @return string
-   */
-  public function getDescription()
-  {
-    return Yii::t('PayeerModule.payeer','Payeer payment module');
-  }
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return Yii::t('PayeerModule.payeer','Payeer payment module');
+    }
 
-  /**
-   * @return string
-   */
-  public function getAuthor()
-  {
-    return Yii::t('PayeerModule.payeer','24-shops');
-  }
+    /**
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return Yii::t('PayeerModule.payeer','24-shops');
+    }
 
-  /**
-   * @return string
-   */
-  public function getAuthorEmail()
-  {
-    return Yii::t('PayeerModule.payeer','24-shops@mail.ru');
-  }
+    /**
+     * @return string
+     */
+    public function getAuthorEmail()
+    {
+        return Yii::t('PayeerModule.payeer','24-shops@mail.ru');
+    }
 
-  /**
-   * @return string
-   */
-  public function getUrl()
-  {
-    return 'https://github.com/24-shops/yupe-payeer';
-  }
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return 'https://github.com/24-shops/yupe-payeer';
+    }
 
-  /**
-   * @return string
-   */
-  public function getIcon()
-  {
-    return 'fa fa-rub';
-  }
+    /**
+     * @return string
+     */
+    public function getIcon()
+    {
+        return 'fa fa-rub';
+    }
 
-  public function init()
-  {
-    parent::init();
+    public function init()
+    {
+        parent::init();
 
-    $this->setImport(
-      [
-        #'application.modules.payeer.models.*',
-        #'application.modules.payeer.components.widgets.*',
-      ]
-    );
-  }
+        $this->setImport(
+            [
+                #'application.modules.payeer.models.*',
+                #'application.modules.payeer.components.widgets.*',
+            ]
+        );
+    }
 }
-
-?>
